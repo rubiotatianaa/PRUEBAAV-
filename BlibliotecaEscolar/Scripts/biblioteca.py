84a15e943f746bba1cfb4223c4a97aee110933f5
Tatiana Rubio <tatiana789rubio@gmail.com>
2025-08-21 08:38:49 -0500

    ejercicio de funciones

--- BlibliotecaEscolar/Scripts/biblioteca.py
+++ BlibliotecaEscolar/Scripts/biblioteca.py
@@ -13,3 +13,29 @@ def agregar_libros(titulo, autor, año, indice):
     }
     libros.append(libro) #append --> para agregar libros a la lista 
     print(f"Se ha agregado el libro con exito Libro'{titulo}'")
+
+    def Eliminar_libro(titulo):
+        for libro in libros:
+            #condicional 
+            if libro['titulo'] == titulo:
+                #Eliminar libro
+                libros.remove(libro)
+                print("Libro eliminado")
+                return
+            print("Libro eliminado")
+    
+    def Editar_Libros(titulo, tituloNuevo, autor, autorNuevo, año, añoNuevo, indice, indiceNuevo):
+        for libro in libros:
+            if libro['tituloNuevo'] == titulo:
+                libro['autorNuevo'] == autor,
+                libro['añoNuevo'] == año,
+                libro['indiceNuevo'] == indice
+
+                #Editar libro
+                tituloNuevo = input("Digite el nombre del nuevo libro")
+                autorNuevo = input("Digite el autor nuevo")
+                añoNuevo = input("Digite el nuevo año")
+                indiceNuevo = input("Digite el indice nuevo")
+
+                
+            
